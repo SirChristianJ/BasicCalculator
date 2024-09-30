3de3b3c893cdc63d7b90ca796b5f970acf9beeb2
SirChristianJ <git@github.com:SirChristianJ/todoApp.git>
2024-09-30 14:15:08 -0400

    Updated methods

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -4,42 +4,45 @@ import java.util.*;
 public class BasicCalculator {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        boolean gameState = true;
-        System.out.print("Enter the first number: ");
-        float firstNum = scanner.nextFloat();
-        //scanner.nextLine();
-
-        System.out.print("Enter the second number: ");
-        float secondNum = scanner.nextFloat();
-        scanner.nextLine();
+        float firstNum = promptForFloat("Enter your first number: ");
+        float secondNum = promptForFloat("Enter a second number: ");
 
         System.out.print("Possible calculations: \n(A)dd\n(S)ubtract\n(M)ultiply\n(D)ivide\nPlease select an option: ");
         String selection = scanner.nextLine();
 
         switch (selection) {
-                case "A":
-                case "a":
-                    add(firstNum,secondNum);
-                    break;
+            case "A":
+            case "a":
+                add(firstNum, secondNum);
+                break;
 
-                case "S":
-                case "s":
-                    subtract(firstNum, secondNum);
-                    break;
+            case "S":
+            case "s":
+                subtract(firstNum, secondNum);
+                break;
 
-                case "M":
-                case "m":
-                    multiply(firstNum, secondNum);
-                    break;
+            case "M":
+            case "m":
+                multiply(firstNum, secondNum);
+                break;
 
-                case "D":
-                case "d":
-                    divide(firstNum, secondNum);
-                    break;
+            case "D":
+            case "d":
+                divide(firstNum, secondNum);
+                break;
 
-                default:
-                    System.out.print("Not a valid option ! Try Again !");
+            default:
+                System.out.print("Not a valid option ! Try Again !");
         }
+    }
+
+    public static float promptForFloat(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(prompt);
+        float num = scanner.nextFloat();
+        scanner.nextLine();
+
+        return num;
     }
 
     public static void add(float num1, float num2){
