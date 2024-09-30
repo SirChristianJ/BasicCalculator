7ae22bcbd6c2cd6cdeb57f34d5cfccc2edcc3c32
SirChristianJ <git@github.com:SirChristianJ/todoApp.git>
2024-09-30 11:58:25 -0400

    Adding methods for each operation

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class BasicCalculator {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        boolean gameState = true;
         System.out.print("Enter the first number: ");
         float firstNum = scanner.nextFloat();
         //scanner.nextLine();
@@ -14,27 +15,47 @@ public class BasicCalculator {
 
         System.out.print("Possible calculations: \n(A)dd\n(S)ubtract\n(M)ultiply\n(D)ivide\nPlease select an option: ");
         String selection = scanner.nextLine();
-        switch(selection) {
-            case "A":
-                System.out.print(firstNum + "+" + secondNum + " = " + (firstNum + secondNum));
-                break;
 
-            case "S":
-                System.out.print(firstNum + "-" + secondNum +  " = " + (firstNum - secondNum));
-                break;
+        switch (selection) {
+                case "A":
+                case "a":
+                    add(firstNum,secondNum);
+                    break;
 
-            case "M":
-                System.out.print(firstNum + "*" + secondNum +  " = " + (firstNum * secondNum));
-                break;
+                case "S":
+                case "s":
+                    subtract(firstNum, secondNum);
+                    break;
 
-            case "D":
-                System.out.print(firstNum + "/" + secondNum +  " = " + (firstNum / secondNum) + "\n");
-                break;
+                case "M":
+                case "m":
+                    multiply(firstNum, secondNum);
+                    break;
 
-            default:
-                System.out.print("Not a valid option ! Try Again !");
+                case "D":
+                case "d":
+                    divide(firstNum, secondNum);
+                    break;
+
+                default:
+                    System.out.print("Not a valid option ! Try Again !");
         }
+    }
 
+    public static void add(float num1, float num2){
+        System.out.print(num1 + " + " + num2 + " = " + (num1 + num2));
+    }
 
+    public static void subtract(float num1, float num2){
+        System.out.print(num1 + " - " + num2 + " = " + (num1 - num2));
+    }
+
+    public static void multiply(float num1, float num2){
+        System.out.print(num1 + " * " + num2 + " = " + (num1 * num2));
+    }
+
+    public static void divide(float num1, float num2){
+        System.out.print(num1 + " / " + num2 + " = " + (num1 / num2));
     }
 }
+
